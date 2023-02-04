ab66b0ab88a8c073d9bc0ba90330f178529105f1
Hinageshi01 <2247001665@qq.com>
2023-02-04 20:54:13 +0800

    Add camera class.

---
+++ Engine/Source/Runtime/Camera/Camera.cpp
@@ -0,0 +1,75 @@
+#include "hnpch.h"
+#include "Camera.h"
+
+namespace Hina
+{
+
+Camera::Camera(const CameraInitializer &init) {
+    Init(init);
+}
+
+void Camera::Init(const CameraInitializer &init) {
+    m_position = init.m_position;
+    m_worldUp = init.m_worldUp;
+    m_yaw = init.m_yaw;
+    m_pitch = init.m_pitch;
+    m_zoom = init.m_zoom;
+    m_speed = init.m_speed;
+    m_sensitivity = init.m_sensitivity;
+
+    Update();
+}
+
+const glm::mat4 Camera::GetViewMatrix() const {
+    return glm::lookAt(m_position, m_position + m_front, m_up);
+}
+
+void Camera::ProcessKeyboard(CameraMovement direction, float deltaTime) {
+    float velocity = m_speed * deltaTime;
+    if(direction == CameraMovement::FORWARD)
+        m_position += m_front * velocity;
+    if(direction == CameraMovement::BACKWARD)
+        m_position -= m_front * velocity;
+    if(direction == CameraMovement::LEFT)
+        m_position -= m_right * velocity;
+    if(direction == CameraMovement::RIGHT)
+        m_position += m_right * velocity;
+    if(direction == CameraMovement::UP)
+        m_position += m_up * velocity;
+    if(direction == CameraMovement::DOWN)
+        m_position -= m_up * velocity;
+}
+
+void Camera::ProcessMouseMovement(float xoffset, float yoffset) {
+    xoffset *= m_sensitivity;
+    yoffset *= m_sensitivity;
+
+    m_yaw += xoffset;
+    m_pitch += yoffset;
+
+    m_pitch = std::clamp(m_pitch, -89.9f, 89.9f);
+
+    Update();
+}
+
+void Camera::ProcessMouseScroll(float yoffset) {
+    m_zoom -= (float)yoffset;
+    if(m_zoom < 1.0f)
+        m_zoom = 1.0f;
+    if(m_zoom > 45.0f)
+        m_zoom = 45.0f;
+}
+
+void Camera::Update() {
+    glm::vec3 front = {
+        cos(glm::radians(m_yaw)) * cos(glm::radians(m_pitch)),
+        sin(glm::radians(m_pitch)),
+        sin(glm::radians(m_yaw)) * cos(glm::radians(m_pitch))
+    };
+
+    m_front = std::move(glm::normalize(front));
+    m_right = std::move(glm::normalize(glm::cross(m_front, m_worldUp)));
+    m_up = std::move(glm::normalize(glm::cross(m_right, m_front)));
+}
+
+} // namespace Hina
---
+++ Engine/Source/Runtime/Camera/Camera.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace Hina
+{
+
+enum class CameraMovement
+{
+    FORWARD, BACKWARD,
+    LEFT, RIGHT,
+    UP, DOWN,
+};
+
+struct CameraInitializer
+{
+    const glm::vec3 m_position = { 0.0f, 0.0f, 0.0f };
+    const glm::vec3 m_worldUp = { 0.0f, 1.0f, 0.0f };
+    const float m_yaw = 0.0f;
+    const float m_pitch = 0.0f;
+    const float m_zoom = 45.0f;
+    const float m_speed = 2.5f;
+    const float m_sensitivity = 0.1f;
+};
+
+class Camera
+{
+public:
+    Camera(const CameraInitializer &init = CameraInitializer());
+    void Init(const CameraInitializer &init);
+
+    const glm::mat4 GetViewMatrix() const;
+
+    void ProcessKeyboard(CameraMovement direction, float deltaTime);
+    void ProcessMouseMovement(float xoffset, float yoffset);
+    void ProcessMouseScroll(float yoffset);
+
+    glm::vec3 m_position = { 0.0f, 0.0f, 0.0f };
+    glm::vec3 m_front = { 0.0f, 0.0f, 1.0f };
+    glm::vec3 m_up = { 0.0f, 1.0f, 0.0f };
+    glm::vec3 m_right = { 1.0f, 0.0f, 0.0f };
+    glm::vec3 m_worldUp = { 0.0f, 1.0f, 0.0f };
+
+    float m_yaw;
+    float m_pitch;
+    float m_zoom;
+
+    float m_speed;
+    float m_sensitivity;
+
+private:
+    void Update();
+};
+
+} // namespace Hina
