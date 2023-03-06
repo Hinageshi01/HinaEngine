c908cb07296dd8a1e88e69d2902a4a9fa245e183
Hinageshi01 <2247001665@qq.com>
2023-03-06 15:32:03 +0800

    Update assimp comment.

--- Engine/Script/MakeAssimp.bat
+++ Engine/Script/MakeAssimp.bat
@@ -8,7 +8,7 @@ echo [ assimp ] path: %ASSIMP_PATH%
 cd %ASSIMP_PATH%
 if not exist build mkdir build
 
-rem https://github.com/assimp/assimp/blob/master/Build.md
+rem https://github.com/assimp/assimp/blob/master/Build.md#cmake-build-options
 cmake -S %ASSIMP_PATH% -B %ASSIMP_PATH%/build -D BUILD_SHARED_LIBS=OFF -D USE_STATIC_CRT=ON
 cmake -S . -B build
 
