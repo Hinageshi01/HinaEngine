f29765572675bc93c8b70c6b45c0d01afaa8a5ad
Hinageshi01 <2247001665@qq.com>
2023-03-28 13:37:55 +0800

    Disable Assimp tests generating.

--- Engine/Script/MakeAssimp.bat
+++ Engine/Script/MakeAssimp.bat
@@ -9,7 +9,7 @@ cd %ASSIMP_PATH%
 if not exist build mkdir build
 
 rem https://github.com/assimp/assimp/blob/master/Build.md#cmake-build-options
-cmake -S %ASSIMP_PATH% -B %ASSIMP_PATH%/build -D BUILD_SHARED_LIBS=OFF -D USE_STATIC_CRT=ON
+cmake -S %ASSIMP_PATH% -B %ASSIMP_PATH%/build -D ASSIMP_BUILD_TESTS=OFF -D BUILD_SHARED_LIBS=OFF -D USE_STATIC_CRT=ON
 cmake -S . -B build
 
 echo.
